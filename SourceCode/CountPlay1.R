745655036f0ebd8642d0eb9e08400f16ecf07df6
Christopher Gandrud <christopher.gandrud@gmail.com>
2013-04-20 14:12:58 +0900

    Testimony Month Totals

--- SourceCode/CountPlay1.R
+++ SourceCode/CountPlay1.R
@@ -1,7 +1,36 @@
 
 
+# Load libraries
+library(lubridate)
+library(DataCombine)
+library(plyr)
+library(ggplot2)
+
 # Set working directory
 setwd("~/Dropbox/Fed_Speeches_Paper/FedSpeech/Data/")
 
-# Import speech count data
-CountData <- read.csv("BaseSpeechCount.csv")
+# Import testimony data
+TestimonyData <- read.csv("TestimonyCount.csv")
+
+# Declare full_data varaiable to be a dat variable
+TestimonyData$Date <- dmy(TestimonyData$full_date)
+
+TestimonyData <- MoveFront(TestimonyData, "Date")
+
+# Month-Year (rounded to closest month) Variable
+TestimonyData$MonthYear <- round_date(TestimonyData$Date, "month")
+
+TestimonyData <- MoveFront(TestimonyData, "MonthYear")
+
+TestimonyData <- TestimonyData[order(TestimonyData$Date), ]
+
+#### Make testimony per month graph ####
+# Create month sums
+TestimonyData$Any <- 1
+TestimonyData <- ddply(TestimonyData, .(MonthYear), transform, MonthTotal = sum(Any)) 
+
+ggplot(TestimonyData, aes(x = MonthYear, y = MonthTotal)) +
+         geom_line() +
+         xlab("") + ylab("Monthly Testimony Totals \n") +
+         theme_bw()
+ 
