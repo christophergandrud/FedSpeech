dd42bddc672f4b3ab72ba152c189afb1ccc05ec6
Christopher Gandrud <christopher.gandrud@gmail.com>
2013-05-27 18:24:45 +0900

    some change point graphs in paper, monthly

--- SourceCode/OddsAndEnds/ChangePointPlay.R
+++ SourceCode/OddsAndEnds/ChangePointPlay.R
@@ -11,18 +11,37 @@ setwd("~/Dropbox/Fed_Speeches_Paper/FedSpeech/Data")
 
 Main <- read.csv("Main.csv")
 
+# Drop if no hearings
+Main <- Main[-1:-2, ]
+Main <- Main[-193:-197, ]
+
 
 #### -------------- Change Point Analysis ----------------- ####
 
 ## Testimony Month Count
-ChangeCount <- cpt.meanvar(Combined$TestCountMonth, method = "BinSeg")
+TestPerMonthTS <- ts(Main$TestCountMonth, start = c(1997, 3), frequency = 12)
+ChangeCount <- cpt.meanvar(TestPerMonth, method = "BinSeg")
 plot(ChangeCount, ylab = "Number of Hearings per Month")
 
+## Members Present Median
+PresTS <- ts(Main$MembPresMedian, start = c(1997, 3), frequency = 12)
+ChangePres <- cpt.mean(PresTS, method = "BinSeg")
+plot(ChangePres, ylab = "Monthly Median Attendance")
 
 ## Laughter Median
-ChangeLaugh <- cpt.meanvar(Combined$LaughMedian)
+LaughTS <- ts(Main$LaughMedian, start = c(1997, 3), frequency = 12)
+ChangeLaugh <- cpt.meanvar(Main$LaughMedian)
 plot(ChangeLaugh, ylab = "Monthly Median Laughter")
 
-## Members Present Median
-ChangePres <- cpt.mean(Combined$MembPresMedian, method = "BinSeg")
-plot(ChangePres, ylab = "Monthly Median Attendance")
+
+#### Median Members Present/Number of Hearings
+attach(Main)
+	MembersHearings <- MembPresMedian/TestCountMonth
+detach(Main)
+
+MembersHearings[is.nan(MembersHearings)] <- 0
+
+MembersTS <- ts(MembersHearings, start = 1997)
+
+ChangePresTest <- cpt.mean(MembersHearings, method = "BinSeg")
+plot(ChangePresTest)
