6949985b7d108d0a0f4c084a19be3f701c177757
Christopher Gandrud <christopher.gandrud@gmail.com>
2013-07-30 17:06:45 +0900

    add 10 topic explore

--- SourceCode/DataCleanMerge/SpeechesTopicClean.R
+++ SourceCode/DataCleanMerge/SpeechesTopicClean.R
@@ -22,7 +22,7 @@ OrgData$Date <- dmy(OrgData$full_date)
 OrgData$MonthYear <- floor_date(OrgData$Date, "month")
 OrgData$QuarterYear <- quarter(OrgData$Date, with_year = TRUE)
 
-#### -------------- Speech Topic Data Clean Up ---------- ######
+#### -------------- Speech Topic Data Clean Up 5 ---------- ######
 setwd("~/Dropbox/Fed_Speeches_Paper/FedSpeech/Data/TopicModeling")
 
 MalletRaw <- read.csv("TopicsinDocs5.csv")
@@ -70,5 +70,65 @@ ToKeep <- c("MonthYear", "QuarterYear", "name", "position_cat", "SpeechID",
             "hearing", "trade_assoc", "non_finance_gov", "nonbuinessadvocacy",
             "social_events", "economic_literacy", "other")
 
-CombClean <- 
+CombClean5 <- CombinedSpeeches[, ToKeep]
 
+# Clean up workspace
+DeleteObj <- setdiff(ls(), c("CombClean5", "OrgData"))
+rm(list = DeleteObj)
+rm(DeleteObj)
+
+
+#### -------------- Speech Topic Data Clean Up 10 ---------- ######
+setwd("~/Dropbox/Fed_Speeches_Paper/FedSpeech/Data/TopicModeling")
+
+MalletRaw <- read.csv("TopicsinDocs10.csv")
+
+# Remove first two columns and last columns (not many observations)
+MalletRaw <- MalletRaw[, c(-1, -2, -14:-18)]
+
+# Clean up variable names
+names(MalletRaw) <- c("SpeechID", "top1", "top1Prop", "top2", "top2Prop",
+                      "top3", "top3Prop", "top4", "top4Prop", "top5",
+                      "top5Prop")
+
+# Extract document ID and order correctly
+MalletRaw$SpeechID <- gsub("\\\\", "", MalletRaw$SpeechID) # Remove Escapes, i.e. \'s
+MalletRaw$SpeechID <- gsub("\\(1\\)May2013SpeechesFedSpeechIndvParsed_20Mayparsed.", "", MalletRaw$SpeechID)
+MalletRaw$SpeechID <- gsub(".txt", "", MalletRaw$SpeechID) # Remove Escapes, i.e. \'s
+
+MalletRaw$SpeechID <- as.numeric(MalletRaw$SpeechID)
+
+MalletOrder <- MalletRaw[order(MalletRaw$SpeechID), ]
+
+# Assign topic labels to data
+TopicLabels <- c("Local.Housing.Dev", "Financial.Markets", "Monetary.Policy", 
+                 "Risk.Regulation", "Housing", "International.Economy", "Banking",
+                 "Prices", "Technology", "Macroeconomics")   
+
+TopicLevels <- 0:9 
+
+TopCols <- c("top1", "top2", "top3", "top4", "top5")
+
+for (i in TopCols){
+  MalletOrder[, i] <- factor(MalletOrder[, i], levels = TopicLevels,
+                            labels = TopicLabels)
+}
+
+#### --------------- Merge Speeches Files ------------- ####
+CombinedSpeeches <- cbind(MalletOrder, OrgData)
+
+# Keep specific variables
+ToKeep <- c("MonthYear", "QuarterYear", "name", "position_cat", "SpeechID",
+            "top1", "top1Prop", "top2", "top2Prop", "top3", "top3Prop",
+            "top4", "top4Prop", "top5", "top5Prop", "bankersfinance",
+            "other_private", "otherregulators", "io", "community_organisations",
+            "thinktank", "press_association", "prof_econ_assoc", "university",
+            "hearing", "trade_assoc", "non_finance_gov", "nonbuinessadvocacy",
+            "social_events", "economic_literacy", "other")
+
+CombClean10 <- CombinedSpeeches[, ToKeep]
+
+# Clean up workspace
+DeleteObj <- setdiff(ls(), c("CombClean5", "CombClean10"))
+rm(list = DeleteObj)
+rm(DeleteObj)
---
+++ SourceCode/TopicModellingAnalysis/SpeechTopicExplore1.R
@@ -0,0 +1,75 @@
+##############
+# Fed Speeches Topics Change Point Explore
+# Christopher Gandrud
+# 30 July 2013
+##############
+
+# Load e.divGG function
+library(devtools)
+source_gist("5675688")
+
+# Depends on data created by SpeechesTopicClean.R
+
+
+LongDataMake <- function(obj, TopicLabels){
+  #### Convert to format for change point analysis
+  TopCols <- c("top1", "top2", "top3", "top4", "top5")
+  TopProp <- c("top1Prop", "top2Prop", "top3Prop", "top4Prop", "top5Prop")
+
+  TempDFi <- data.frame()
+  for (i in TopCols){
+      Temp <- obj[, c("MonthYear", "QuarterYear", "SpeechID", i)]
+      names(Temp) <- c("MonthYear", "QuarterYear", "SpeechID", "Topic")
+      TempDFi <- rbind(TempDFi, Temp)
+  }
+
+  TempDFu <- data.frame()
+  for (u in TopProp){
+    Temp <- data.frame(obj[, c(u)])
+    names(Temp) <- c("TopicProp")
+    TempDFu <- rbind(TempDFu, Temp)
+  }
+
+  LongTopic <- cbind(TempDFi, TempDFu)
+
+  TopicLabels <- c("Financial.Markets", "Macroeconomics", "Monetary.Policy", 
+                   "International.Economy",  "Local.Housing.Dev", 
+                   "Banking.Regulation") 
+
+  TopicWide <- LongTopic[, 1:3]
+  for (v in TopicLabels){
+    Temp <- subset(LongTopic, Topic == v)
+    Temp <- Temp[, -4]
+    names(Temp) <- c("MonthYear", "QuarterYear", "SpeechID", v)
+    TopicWide <- merge(TopicWide, Temp, 
+                       by = c("MonthYear", "QuarterYear", "SpeechID"), all = TRUE )
+  }
+
+  TopicWide <- TopicWide[!duplicated(TopicWide[, 1:3]), ]
+
+  ## Code NA as 0
+  TopicWide[is.na(TopicWide)] <- 0
+  TopicWide
+}
+
+TopicLabels5 <- c("Financial.Markets", "Macroeconomics", "Monetary.Policy", 
+                 "International.Economy",  "Local.Housing.Dev", 
+                 "Banking.Regulation") 
+
+TopicLabels10 <- c("Local.Housing.Dev", "Financial.Markets", "Monetary.Policy", 
+                 "Risk.Regulation", "Housing", "International.Economy", "Banking",
+                 "Prices", "Technology", "Macroeconomics")  
+
+TopicWide5 <- LongDataMake(obj = CombClean5, TopicLabels = TopicLabels5)
+TopicWide10 <- LongDataMake(obj = CombClean10, TopicLabels = TopicLabels10)
+
+
+
+###### ------- Preliminary Change Point ---------- ####
+
+#### 5
+CleanNames <- c("Financial Markets", "Macroeconomics", "Monetary Policy", 
+                "International Economy",  "Local Housing and Dev", 
+                "Banking Regulation")
+e.divGG(data = TopicWide, Vars = TopicLabels, TimeVar = "MonthYear", 
+        sig.lvl = 0.05, R = 999, min.size = 6, Titles = CleanNames)
