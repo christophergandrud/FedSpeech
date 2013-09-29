237354a05d8d61fe7f281694a1b5ec80f354879c
Christopher Gandrud <christopher.gandrud@gmail.com>
2013-09-29 12:03:21 +0200

    uPreProcess done

--- SourceCode/ExpAgenda/PreProcessData.R
+++ SourceCode/ExpAgenda/PreProcessData.R
@@ -1,73 +1,34 @@
 #############
 # Prepare speeches for text analysis
 # Christopher Gandrud
-# 28 September 2013
+# 29 September 2013
 #############
 
-library(tm)
-library(SnowballC)
 library(ExpAgenda)
+library(stringr)
 
-# Import speeches
+RemoveTitle <- function(data){
+  data[, "name"] <- gsub(pattern = "Governor†", "", data[, "name"])
+  data[, "name"] <- gsub(pattern = "Governor", "", data[, "name"])
+  data[, "name"] <- gsub(pattern = "Chairman†", "", data[, "name"])
+  data[, "name"] <- gsub(pattern = "Chairman", "", data[, "name"])
+  data[, "name"] <- gsub(pattern = "Vice Chair†", "", data[, "name"])
+  data[, "name"] <- gsub(pattern = "Vice Chair", "", data[, "name"])
+  data[, "name"] <- str_trim(data[, "name"])
+  data
+}
+
 setwd("~/Dropbox/Fed_Speeches_Data/May2013Speeches/FedSpeechIndvParsed_20May/")
-FileList <- list.files(pattern = "*.txt")
-Texts <- lapply(FileList, readLines)
-TextsV <- as.vector(Texts)
 
-# Basic clean
-TextsV <- tolower(TextsV)
-TextsV <- removeNumbers(TextsV)
-TextsV <- removePunctuation(TextsV)
+MetaData <- read.csv("~/Dropbox/Fed_Speeches_Paper/FedSpeech/Data/Raw/FedSpeechesVersionMay2013.csv",
+                     stringsAsFactors = FALSE)
 
-# Remove stop words
-StopWords <- stopwords(kind = "en")
-TextsV <- removeWords(TextsV, StopWords)
+MetaData <- RemoveTitle(MetaData)
 
-TextsV <- stripWhitespace(TextsV)
+DocTerm <- PreProcess(textsPattern = "*.txt", authorsDF = MetaData,
+                      AuthorCol = "name", removeAuthors = "Brian F. Madigan", 
+                      sparse = 0.8)
 
-# Import speaker/date meta data
-Meta <- read.csv("~/Dropbox/Fed_Speeches_Paper/FedSpeech/Data/Raw/FedSpeechesVersionMay2013.csv",
-                 stringsAsFactors = FALSE)
-MetaSub <- Meta[, c("full_date", "name")]
-
-# Bind into one data frame
-Full <- cbind(MetaSub, TextsV)
-
-#### Create author matrix ####
-# Order by author
-FullOrd <- Full[order(Full$name), ]
-
-# Create author matrix
-AuthorsRaw <- as.data.frame(FullOrd$name)
-AuthorsRaw$ID <- row.names(AuthorsRaw)
-
-First <- by(AuthorsRaw, AuthorsRaw[, 1], head, n = 1)
-Last <- by(AuthorsRaw, AuthorsRaw[, 1], tail, n = 1)
-
-First <- do.call("rbind", as.list(First))
-Last <- do.call("rbind", as.list(Last))
-
-Authors <- cbind(First[, 1:2], Last[, 2])
-
-names(Authors) <- c("name", "first", "last")
-Authors <- as.matrix(Authors)
-
-#### Create document term matrix ####
-# Convert to corpus
-# FullCorp <- Corpus(DataframeSource(Full))
-FullCorp <- Corpus(VectorSource(FullOrd$TextsV))
-
-# Stemming
-Stems <- tm_map(FullCorp, stemDocument)
-
-# Create term document matrix
-TermDoc <- DocumentTermMatrix(Stems)
-
-# Remove sparse terms
-TermDocS <- removeSparseTerms(TermDoc, 0.9)
-
-
-
-
-
+## Run text analysis
+TestEA <- ExpAgendaVonmon(obj = DocTerm)
 
