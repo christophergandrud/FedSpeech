599a444f1e604c28f7173f6f9e1601f7389a9aad
christophergandrud <christopher.gandrud@gmail.com>
2015-05-07 17:14:51 +0200

    lda vis first go

---
+++ SourceCode/TopicModels/topic_models_v1.R
@@ -0,0 +1,67 @@
+###########################
+# LDA Topic model explore
+# Christopher Gandrud
+# MIT License
+###########################
+
+# Load required packages
+library(tm)
+library(dplyr)
+library(stringi)
+library(topicmodels)
+library(LDAvis)
+
+# Set working directory
+setwd('~/Dropbox/Fed_Speeches_Data/fed.text.parsed/')
+
+# Load and process corpus
+corpus <- Corpus(DirSource()) %>%
+    tm_map(content_transformer(tolower)) %>%
+    tm_map(removeWords, stopwords('english'), mc.cores = 1) %>%
+    tm_map(stemDocument, mc.cores = 1) %>%
+    tm_map(stripWhitespace) %>%
+    tm_map(removePunctuation, mc.cores = 1) %>%
+    tm_map(removeNumbers, mc.cores = 1)
+
+# Create document-term matrix for LDA
+doc_term <- DocumentTermMatrix(corpus)
+
+#### Run topic models for a range of topics ####
+topic_number <-
+for (i in c(3, 5, 7, 10, 15, 20)) {
+    message(sprintf('Running %s', i))
+    assign(sprintf('lda_%s', i), LDA(doc_term, k = i, seed = 1001))
+}
+
+#### Compare log-likelihood per number of topics, to assess model fit ####
+lda_out <- sprintf('lda_%s', topic_number)
+log_lik <- vector()
+for (i in lda_out) log_lik <- c(log_link, logLik(i))
+
+plot(topic_number, log_lik)
+
+#### Visualise topics and terms ####
+# Choose fitted model to visualise
+to_visualise <- lda_10 # 10 topic model
+
+# Create json objected needed for LDAvis
+phi <- posterior(to_visualise)$terms %>% as.matrix
+theta <- posterior(to_visualise)$topics %>% as.matrix
+vocab <- colnames(phi)
+doc_length <- vector()
+for (i in 1:length(corpus)) {
+    temp <- paste(corpus[[i]]$content, collapse = ' ')
+    doc_length <- c(doc_length, stri_count(temp, regex = '\\S+'))
+}
+temp_frequency <- inspect(doc_term)
+freq_matrix <- data.frame(ST = colnames(temp_frequency), Freq = colSums(temp_frequency))
+row.names(freq_matrix) <- NULL
+rm(temp_frequency)
+
+json_lda <- createJSON(phi = phi, theta = theta,
+                       vocab = vocab,
+                       doc.length = doc_length,
+                       term.frequency = freq_matrix$Freq)
+
+# Visualise with LDAvis
+serVis(json_lda, open.browser = T)
